f3c4b1f1e182dd2747ed303570dd2d26cd2a7367
Zachariah Reeves <56128534+kingzachy20@users.noreply.github.com>
2019-12-12 09:38:48 -0800

    finished it

--- Prototype-2working/Assets/Scripts/PlayerController.cs
+++ Prototype-2working/Assets/Scripts/PlayerController.cs
@@ -6,8 +6,8 @@ public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
     public float speed = 10.0f;
-    public float leftBoundary = -10.0f;
-    public float rightBouandary = 10.0f;
+    public float leftBoundary = -32.0f;
+    public float rightBouandary = 32.0f;
 
     public GameObject projectilePrefab;
    
@@ -28,7 +28,7 @@ public class PlayerController : MonoBehaviour
 
         if (transform.position.x > rightBouandary)
         {
-            transform.position = new Vector3(10, transform.position.y, transform.position.z);
+            transform.position = new Vector3(rightBouandary, transform.position.y, transform.position.z);
         }
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
